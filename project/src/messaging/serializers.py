ab53e16088794c3353a9cc93c3f74c3090e382d0
Salik Tariq <saliktariq@icloud.com>
2021-03-30 12:53:06 +0100

    Messaging app serializers.py completed

---
+++ project/src/messaging/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Messages, Feedback, Topics
+
+class TopicsSerializer(serializers.ModelSerializer):
+    class Meta:
+      model = Topics
+      fields = ['topic_name']
+
+
+class FeedbackSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Feedback
+        fields = ['is_liked', 'is_disliked', 'comment', 'username', 'user_id']
+
+
+class MessagesSerializer(serializers.ModelSerializer):
+    feedbacks = FeedbackSerializer(source='feedback_set', many=True)
+    class Meta:
+        model = Messages
+        fields = ['post_identifier', 'topic', 'title', 'message', 'creation_timestamp', 'expiration_timestamp', 'is_live', 'username', 'user_id', 'feedbacks']
